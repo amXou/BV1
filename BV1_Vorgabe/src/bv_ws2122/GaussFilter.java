ba355cb3a338d796425bc58b6a4d87f9d5418413
amXou <82817877+amXou@users.noreply.github.com>
2021-10-15 22:22:09 +0200

    Add files via upload
    GaussFilter with Edge treatment

--- BV1_Vorgabe/src/bv_ws2122/GaussFilter.java
+++ BV1_Vorgabe/src/bv_ws2122/GaussFilter.java
@@ -27,8 +27,8 @@ public class GaussFilter {
 		double sum = 0;
 		for (int x = 0; x < kernelSize; x ++) {
 			for (int y = 0; y < kernelSize; y++) {
-				double d = (Math.sqrt((Math.pow(Math.abs(y-hotspot), 2) + (Math.pow(Math.abs(y-hotspot), 2)))));
-				kernel[x][y] = Math.pow(Math.E, (-Math.pow(d, 2)) / (2 * Math.pow(sigma, 2)));
+				double d = (Math.sqrt((Math.pow(Math.abs(y-hotspot), 2) + (Math.pow(Math.abs(x-hotspot), 2))))); // sqrt(delta y^2 + delta x^2) (Abstand des aktuellen Pixels zum Hotspot
+				kernel[x][y] = Math.pow(Math.E, ((-Math.pow(d, 2)) / (2 * Math.pow(sigma, 2))));
 				sum = sum + kernel[x][y];
 			}
 		}
@@ -69,11 +69,12 @@ public class GaussFilter {
 				for(int x = 0; x < src.width; x++) {
 					for(int y = 0; y < src.height; y++) {
 						int pos = y * src.width + x;
-						if(x < hotspot || x >= src.width - hotspot
+				/*		if(x < hotspot || x >= src.width - hotspot
 								|| y < hotspot || y >= src.height - hotspot) {
 							//Rand zunächst auf scharz setzen
-							dst.argb[pos] = 0xff000000;
-						} else {
+							dst.argb[pos] = 0xff000000;	
+						} else { 
+				*/
 							int newvalue = 0;
 							int rn = 0;
 							int gn = 0;
@@ -81,8 +82,41 @@ public class GaussFilter {
 							//durch den Kernel loopen
 							for(int k = -hotspot; k <= hotspot; k++) {
 								for(int l = -hotspot; l <= hotspot; l++) {
-									//newvalue = (int) (newvalue + src.argb[(x + k) + (y+l) * src.width] * kernel[k+hotspot][l+hotspot]);
-									int argb = src.argb[(x + k) + (y+l) * src.width];
+									//Randbehandlung
+									int argb = 0;
+									if (x+k >= src.width | x+k < 0 | y+l >= src.height| y+l < 0){
+										//kernelelemente in -y richtung = -y + 1 wert
+										if(x+k >= src.width && (y+l)<0) { //top right corner
+											argb = src.argb[(src.width - 1)];
+										}
+										if(x+k >= src.width && (y+l)<src.height && (y+l)>=0) { //right side
+											argb = src.argb[(src.width -1) + src.width * (y+l)];
+										}
+										if(x+k >= src.width && (y+l)>=src.height) { //bottom right corner
+											argb = src.argb[(src.width - 1) + src.width * (src.height-1)];
+										}
+										if(y+l >= src.height && (x+k)<src.width && (x+k)>=0) { //bottom side
+											argb = src.argb[(src.width -1) + src.width * (src.height-1)];
+										}
+										if(y+l >= src.height && (x+k)<0) { //bottom left corner
+											argb = src.argb[1 + src.width * (src.height-1)];
+										}
+										if(x+k < 0 && (y+l)<src.height && (y+l)>=0) { //left side
+											argb = src.argb[1 + src.width * (y+l)];
+										}
+										if(y+l < 0 && (x+k)<0) { //top left corner
+											argb = src.argb[0];
+										}
+										if(y+l < 0 && (x+k)<src.width && (x+k)>=0) { //top side
+											argb = src.argb[(x+k)];
+										}
+										//kernelelemente in -x richtung = -x + 1 wert
+									}
+									else {
+										//newvalue = (int) (newvalue + src.argb[(x + k) + (y+l) * src.width] * kernel[k+hotspot][l+hotspot]);
+										argb = src.argb[(x + k) + (y+l) * src.width];
+										
+									}
 									int r = (argb >> 16) & 0xff;
 									int g = (argb >>  8) & 0xff;
 									int b =  argb & 0xff; 
@@ -91,7 +125,7 @@ public class GaussFilter {
 									gn = (int) (gn + g * kernel[k+hotspot][l+hotspot]);
 									bn = (int) (bn + b * kernel[k+hotspot][l+hotspot]);
 								}
-							}
+				//			}
 							//grayscale
 							newvalue = (rn+gn+bn)/3;
 							
